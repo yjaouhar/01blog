c74bb1eb0b1ee9c8077a60203898e0f4a902d87a
Yassine Jaouhary <yassinejaouhary82@gmail.com>
2025-10-29 09:06:17 +0100

    initial project

---
+++ src/main/java/com/_blog/app/AppApplication.java
@@ -0,0 +1,13 @@
+package com._blog.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/_blog/app/AppApplicationTests.java
@@ -0,0 +1,13 @@
+package com._blog.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
