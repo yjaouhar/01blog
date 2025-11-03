8d2c5cb6233d25a329724488e2b83ffcd7c2b9d0
yassine jaouhary <yassinejaouhary82@gmail.com>
2025-11-03 02:50:07 +0100

    jwt helper

--- src/main/java/com/_blog/app/config/JwtHelper.java
+++ src/main/java/com/_blog/app/config/JwtHelper.java
@@ -1,7 +1,44 @@
 package com._blog.app.config;
 
+import java.util.Date;
+import java.util.Map;
+import java.util.function.Function;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import com._blog.app.entities.UserAccount;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+
+@Component
 public class JwtHelper {
-    
-    
+
+    @Value("${jwt.secret}")
+    private String JWT_SECRET;
+
+    public String generateToken(Map<String, Object> extraClaims, UserAccount userAccount) {
+        return Jwts.builder()
+                .claims(extraClaims)
+                .subject(userAccount.getUsername())
+                .issuedAt(new Date(System.currentTimeMillis()))
+                .expiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 24 * 14))
+                .signWith(getSignInkey())
+                .compact();
+    }
+
+    public <T> T extraClaims(String token, Function<Claims, T> claimsResolve) {
+        final Claims claims = extractAllClaims(token);
+        return claimsResolve.apply(claims);
+    }
+
+    private Claims extractAllClaims(String token) {
+        return Jwts
+                .parser()
+                .verifyWith(getSignInkey())
+                .build()
+                .parseSignedClaims(token)
+                .getPayload();
+    }
 }
